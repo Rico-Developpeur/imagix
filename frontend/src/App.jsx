addd1a4efb4310c95e71c8013bdabe03ba709e25
Rico-Developpeur <eric.vieira@sncf.fr>
2022-09-11 15:59:58 +0200

    imagix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
-import Home from "@pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Prestations from "./pages/Prestations/Prestations";
+import Animations from "./pages/Animations/Animation";
+import Contact from "./pages/Contact/Contact";
+import Galerie from "./pages/Galerie/Galerie";
+import Atelier from "./pages/Atelier/Atelier";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/prestations" element={<Prestations />} />
+          <Route path="/animations" element={<Animations />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/galerie" element={<Galerie />} />
+          <Route path="/atelier" element={<Atelier />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
